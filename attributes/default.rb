812b6017e65898c40dc747cad755e103fe09365a
Osvaldo Matos-Junior <tupy@jusbrasil.com.br>
2013-03-19 16:00:15 -0300

    Password protection added to admin stats.

--- attributes/default.rb
+++ attributes/default.rb
@@ -31,6 +31,7 @@ default['haproxy']['ssl_httpchk'] = nil
 default['haproxy']['enable_admin'] = true
 default['haproxy']['admin']['address_bind'] = "127.0.0.1"
 default['haproxy']['admin']['port'] = 22002
+default['haproxy']['admin']['password'] = nil
 default['haproxy']['pid_file'] = "/var/run/haproxy.pid"
 
 default['haproxy']['defaults_options'] = ["httplog", "dontlognull", "redispatch"]
